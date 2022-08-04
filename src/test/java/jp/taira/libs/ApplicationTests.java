b46ac74fcbe91f6ff7837f95033ac8b978986978
Yuma Taira <bbytyk2210@gmail.com>
2022-08-05 00:42:29 +0900

    add environment

---
+++ src/test/java/jp/taira/libs/ApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.jp.taira.libs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTests {
+
+    @Test
+	void contextLoads() {
+	}
+    
+}
