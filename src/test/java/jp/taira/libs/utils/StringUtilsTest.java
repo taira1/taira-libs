7f5287b94b19b9ac96908827b37dd179b6acfb00
Yuma Taira <bbytyk2210@gmail.com>
2022-08-10 08:55:07 +0900

    update StringUtilsTest

--- src/test/java/jp/taira/libs/utils/StringUtilsTest.java
+++ src/test/java/jp/taira/libs/utils/StringUtilsTest.java
@@ -144,4 +144,206 @@ public class StringUtilsTest {
         assertTrue(StringUtils.endsWithIgnoreCase("ABC", "c"));
         assertTrue(StringUtils.endsWithIgnoreCase("ABC", "C"));
     }
+
+    @Test
+    public void trimTest() throws Exception {
+        { /* 指定あり(文字) */
+            assertNull(StringUtils.trim(null, "a"));
+            assertEquals(StringUtils.trim("", "a"), "");
+            assertEquals(StringUtils.trim("  ", "a"), "  ");
+            assertEquals(StringUtils.trim(" 　", "a"), " 　");
+            assertEquals(StringUtils.trim("　 ", "a"), "　 ");
+
+            assertEquals(StringUtils.trim("AAA", "a"), "AAA");
+            assertEquals(StringUtils.trim("A A A", "a"), "A A A");
+            assertEquals(StringUtils.trim(" A A A ", "a"), " A A A ");
+            assertEquals(StringUtils.trim(" 　A A A 　", "a"), " 　A A A 　");
+            assertEquals(StringUtils.trim("　 A A A　 ", "a"), "　 A A A　 ");
+            assertEquals(StringUtils.trim("　　A A A　　", "a"), "　　A A A　　");
+            assertEquals(StringUtils.trim("    A A A    ", "a"), "    A A A    ");
+
+            assertEquals(StringUtils.trim("A　A　A", "a"), "A　A　A");
+            assertEquals(StringUtils.trim(" A　A　A ", "a"), " A　A　A ");
+            assertEquals(StringUtils.trim(" 　A　A　A 　", "a"), " 　A　A　A 　");
+            assertEquals(StringUtils.trim("　 A　A　A　 ", "a"), "　 A　A　A　 ");
+            assertEquals(StringUtils.trim("　　A　A　A　　", "a"), "　　A　A　A　　");
+            assertEquals(StringUtils.trim("    A　A　A    ", "a"), "    A　A　A    ");
+
+            assertEquals(StringUtils.trim("a", "a"), "");
+            assertEquals(StringUtils.trim("aaa", "a"), "");
+
+            assertEquals(StringUtils.trim("abc", "a"), "bc");
+            assertEquals(StringUtils.trim("abcabcabc", "a"), "bcabcabc");
+            assertEquals(StringUtils.trim(" abc ", "a"), " abc ");
+            assertEquals(StringUtils.trim(" abcabcabc ", "a"), " abcabcabc ");
+            assertEquals(StringUtils.trim("abc abc abc", "a"), "bc abc abc");
+            assertEquals(StringUtils.trim("abc abc", "a"), "bc abc");
+        }
+
+        { /* 指定あり(文字列) */
+            assertNull(StringUtils.trim(null, "a"));
+            assertEquals(StringUtils.trim("", "abc"), "");
+            assertEquals(StringUtils.trim("  ", "abc"), "  ");
+            assertEquals(StringUtils.trim(" 　", "abc"), " 　");
+            assertEquals(StringUtils.trim("　 ", "abc"), "　 ");
+
+            assertEquals(StringUtils.trim("AAA", "abc"), "AAA");
+            assertEquals(StringUtils.trim("A A A", "abc"), "A A A");
+            assertEquals(StringUtils.trim(" A A A ", "abc"), " A A A ");
+            assertEquals(StringUtils.trim(" 　A A A 　", "abc"), " 　A A A 　");
+            assertEquals(StringUtils.trim("　 A A A　 ", "abc"), "　 A A A　 ");
+            assertEquals(StringUtils.trim("　　A A A　　", "abc"), "　　A A A　　");
+            assertEquals(StringUtils.trim("    A A A    ", "abc"), "    A A A    ");
+
+            assertEquals(StringUtils.trim("A　A　A", "abc"), "A　A　A");
+            assertEquals(StringUtils.trim(" A　A　A ", "abc"), " A　A　A ");
+            assertEquals(StringUtils.trim(" 　A　A　A 　", "abc"), " 　A　A　A 　");
+            assertEquals(StringUtils.trim("　 A　A　A　 ", "abc"), "　 A　A　A　 ");
+            assertEquals(StringUtils.trim("　　A　A　A　　", "abc"), "　　A　A　A　　");
+            assertEquals(StringUtils.trim("    A　A　A    ", "abc"), "    A　A　A    ");
+
+            assertEquals(StringUtils.trim("a", "abc"), "a");
+            assertEquals(StringUtils.trim("aaa", "abc"), "aaa");
+
+            assertEquals(StringUtils.trim("abc", "abc"), "");
+            assertEquals(StringUtils.trim("abcabcabc", "abc"), "");
+            assertEquals(StringUtils.trim(" abc ", "abc"), " abc ");
+            assertEquals(StringUtils.trim(" abcabcabc ", "abc"), " abcabcabc ");
+            assertEquals(StringUtils.trim("abc abc abc", "abc"), " abc ");
+            assertEquals(StringUtils.trim("abc abc", "abc"), " ");
+        }
+
+        { /* 指定なし=空白(全角、半角) */
+            assertNull(StringUtils.trim(null));
+            assertEquals(StringUtils.trim(""), "");
+            assertEquals(StringUtils.trim("  "), "");
+            assertEquals(StringUtils.trim(" 　"), "");
+            assertEquals(StringUtils.trim("　 "), "");
+
+            assertEquals(StringUtils.trim("AAA"), "AAA");
+            assertEquals(StringUtils.trim("A A A"), "A A A");
+            assertEquals(StringUtils.trim(" A A A "), "A A A");
+            assertEquals(StringUtils.trim(" 　A A A 　"), "A A A");
+            assertEquals(StringUtils.trim("　 A A A　 "), "A A A");
+            assertEquals(StringUtils.trim("　　A A A　　"), "A A A");
+            assertEquals(StringUtils.trim("    A A A    "), "A A A");
+
+            assertEquals(StringUtils.trim("A　A　A"), "A　A　A");
+            assertEquals(StringUtils.trim(" A　A　A "), "A　A　A");
+            assertEquals(StringUtils.trim(" 　A　A　A 　"), "A　A　A");
+            assertEquals(StringUtils.trim("　 A　A　A　 "), "A　A　A");
+            assertEquals(StringUtils.trim("　　A　A　A　　"), "A　A　A");
+            assertEquals(StringUtils.trim("    A　A　A    "), "A　A　A");
+        }
+    }
+
+    public void isAsciiTest() throws Exception {
+        assertFalse(StringUtils.isAscii(null));
+        assertFalse(StringUtils.isAscii(""));
+        assertTrue(StringUtils.isAscii("  "));
+
+        assertTrue(StringUtils.isAscii("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+        assertTrue(StringUtils.isAscii("AAA"));
+        assertTrue(StringUtils.isAscii("abcdefghijklmnopqrstuvwxyz"));
+        assertTrue(StringUtils.isAscii("aaa"));
+        assertTrue(StringUtils.isAscii("  aaa"));
+        assertTrue(StringUtils.isAscii("aaa  "));
+
+        assertTrue(StringUtils.isAscii("012"));
+        assertTrue(StringUtils.isAscii("0123456789"));
+        assertTrue(StringUtils.isAscii("  012"));
+        assertTrue(StringUtils.isAscii("  0123456789"));
+        assertTrue(StringUtils.isAscii("012  "));
+        assertTrue(StringUtils.isAscii("0123456789  "));
+
+        assertTrue(StringUtils.isAscii("012aaa"));
+        assertTrue(StringUtils.isAscii("aaa012"));
+        assertTrue(StringUtils.isAscii("012 aaa"));
+        assertTrue(StringUtils.isAscii("aaa 012"));
+        assertTrue(StringUtils.isAscii("012AAA"));
+        assertTrue(StringUtils.isAscii("AAA012"));
+        assertTrue(StringUtils.isAscii("012 AAA"));
+        assertTrue(StringUtils.isAscii("AAA 012"));
+
+        assertFalse(StringUtils.isAscii("　"));
+        assertFalse(StringUtils.isAscii("０１２３４５６７８９"));
+        assertFalse(StringUtils.isAscii("ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ"));
+        assertFalse(StringUtils.isAscii("ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ"));
+        assertFalse(StringUtils.isAscii("あいうえお"));
+        assertFalse(StringUtils.isAscii("アイウエオ"));
+    }
+
+    @Test
+    public void isAlphaTest() throws Exception {
+        assertFalse(StringUtils.isAlpha(null));
+        assertFalse(StringUtils.isAlpha(""));
+        assertFalse(StringUtils.isAlpha("  "));
+
+        assertTrue(StringUtils.isAlpha("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+        assertTrue(StringUtils.isAlpha("AAA"));
+        assertTrue(StringUtils.isAlpha("abcdefghijklmnopqrstuvwxyz"));
+        assertTrue(StringUtils.isAlpha("aaa"));
+        assertFalse(StringUtils.isAlpha("  aaa"));
+        assertFalse(StringUtils.isAlpha("aaa  "));
+
+        assertFalse(StringUtils.isAlpha("012"));
+        assertFalse(StringUtils.isAlpha("0123456789"));
+        assertFalse(StringUtils.isAlpha("  012"));
+        assertFalse(StringUtils.isAlpha("  0123456789"));
+        assertFalse(StringUtils.isAlpha("012  "));
+        assertFalse(StringUtils.isAlpha("0123456789  "));
+
+        assertFalse(StringUtils.isAlpha("012aaa"));
+        assertFalse(StringUtils.isAlpha("aaa012"));
+        assertFalse(StringUtils.isAlpha("012 aaa"));
+        assertFalse(StringUtils.isAlpha("aaa 012"));
+        assertFalse(StringUtils.isAlpha("012AAA"));
+        assertFalse(StringUtils.isAlpha("AAA012"));
+        assertFalse(StringUtils.isAlpha("012 AAA"));
+        assertFalse(StringUtils.isAlpha("AAA 012"));
+
+        assertFalse(StringUtils.isAlpha("　"));
+        assertFalse(StringUtils.isAlpha("０１２３４５６７８９"));
+        assertFalse(StringUtils.isAlpha("ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ"));
+        assertFalse(StringUtils.isAlpha("ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ"));
+        assertFalse(StringUtils.isAlpha("あいうえお"));
+        assertFalse(StringUtils.isAlpha("アイウエオ"));
+    }
+
+    @Test
+    public void isHalfAlphaNumericTest() throws Exception {
+        assertFalse(StringUtils.isHalfAlphaNumeric(null));
+        assertFalse(StringUtils.isHalfAlphaNumeric(""));
+        assertFalse(StringUtils.isHalfAlphaNumeric("  "));
+
+        assertTrue(StringUtils.isHalfAlphaNumeric("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+        assertTrue(StringUtils.isHalfAlphaNumeric("AAA"));
+        assertTrue(StringUtils.isHalfAlphaNumeric("abcdefghijklmnopqrstuvwxyz"));
+        assertTrue(StringUtils.isHalfAlphaNumeric("aaa"));
+        assertFalse(StringUtils.isHalfAlphaNumeric("  aaa"));
+        assertFalse(StringUtils.isHalfAlphaNumeric("aaa  "));
+
+        assertTrue(StringUtils.isHalfAlphaNumeric("012"));
+        assertTrue(StringUtils.isHalfAlphaNumeric("0123456789"));
+        assertFalse(StringUtils.isHalfAlphaNumeric("  012"));
+        assertFalse(StringUtils.isHalfAlphaNumeric("  0123456789"));
+        assertFalse(StringUtils.isHalfAlphaNumeric("012  "));
+        assertFalse(StringUtils.isHalfAlphaNumeric("0123456789  "));
+
+        assertTrue(StringUtils.isHalfAlphaNumeric("012aaa"));
+        assertTrue(StringUtils.isHalfAlphaNumeric("aaa012"));
+        assertFalse(StringUtils.isHalfAlphaNumeric("012 aaa"));
+        assertFalse(StringUtils.isHalfAlphaNumeric("aaa 012"));
+        assertTrue(StringUtils.isHalfAlphaNumeric("012AAA"));
+        assertTrue(StringUtils.isHalfAlphaNumeric("AAA012"));
+        assertFalse(StringUtils.isHalfAlphaNumeric("012 AAA"));
+        assertFalse(StringUtils.isHalfAlphaNumeric("AAA 012"));
+
+        assertFalse(StringUtils.isHalfAlphaNumeric("　"));
+        assertFalse(StringUtils.isHalfAlphaNumeric("０１２３４５６７８９"));
+        assertFalse(StringUtils.isHalfAlphaNumeric("ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ"));
+        assertFalse(StringUtils.isHalfAlphaNumeric("ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ"));
+        assertFalse(StringUtils.isHalfAlphaNumeric("あいうえお"));
+        assertFalse(StringUtils.isHalfAlphaNumeric("アイウエオ"));
+    }
 }
