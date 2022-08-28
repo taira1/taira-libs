a17a27d4e1d6ec7637c030a5fd6177e5a9f723a8
Yuma Taira <bbytyk2210@gmail.com>
2022-08-29 08:37:48 +0900

    update ExcelUtilstTest

--- src/test/java/jp/taira/libs/utils/ExcelUtilsTest.java
+++ src/test/java/jp/taira/libs/utils/ExcelUtilsTest.java
@@ -322,4 +322,146 @@ public class ExcelUtilsTest {
             assertEquals(workbook.getNumberOfSheets(), sheetNameArray.length);
         }
     }
+
+    @Test
+    public void copySheetTest() {
+        { /* null */
+            final Sheet sheet = ExcelUtils.copySheet(null, null, null);
+
+            assertNull(sheet);
+        }
+
+        { /* null */
+            final Sheet sheet = ExcelUtils.copySheet(null, "templateSheetName", "sheetName");
+
+            assertNull(sheet);
+        }
+
+        { /* シート名(null) */
+            final Workbook workbook = ExcelUtils.getWorkbook(getResourceFile("testExcel/test.xls"));
+            final Sheet sheet = ExcelUtils.copySheet(workbook, null, "sheetName");
+
+            assertNull(sheet);
+        }
+
+        { /* シート名(null) */
+            final Workbook workbook = ExcelUtils.getWorkbook(getResourceFile("testExcel/test.xls"));
+            final Sheet sheet = ExcelUtils.copySheet(workbook, "templateSheetName", null);
+
+            assertNull(sheet);
+        }
+
+        { /* シート名(null) */
+            final Workbook workbook = ExcelUtils.getWorkbook(getResourceFile("testExcel/test.xls"));
+            final Sheet sheet = ExcelUtils.copySheet(workbook, "Sheet1", null);
+
+            assertNull(sheet);
+        }
+
+        { /* シート名(空) */
+            final Workbook workbook = ExcelUtils.getWorkbook(getResourceFile("testExcel/test.xls"));
+            final Sheet sheet = ExcelUtils.copySheet(workbook, "Sheet1", "");
+
+            assertNull(sheet);
+        }
+
+        { /* 通常 */
+            final Workbook workbook = ExcelUtils.getWorkbook(getResourceFile("testExcel/test.xls"));
+            final int sheetNum = Objects.requireNonNull(workbook).getNumberOfSheets();
+            final Sheet sheet = ExcelUtils.copySheet(workbook, "Sheet1", "Sheet9");
+
+            assertNotNull(sheet);
+            assertEquals(sheet.getSheetName(), "Sheet9");
+            assertNotNull(workbook.getSheet("Sheet9"));
+            // コピー元は存在する。
+            assertNotNull(workbook.getSheet("Sheet1"));
+            assertEquals(workbook.getNumberOfSheets(), sheetNum + 1);
+        }
+
+        { /* シート名(null) */
+            final Workbook workbook = ExcelUtils.getWorkbook(getResourceFile("testExcel/test.xlsx"));
+            final Sheet sheet = ExcelUtils.copySheet(workbook, null, "sheetName");
+
+            assertNull(sheet);
+        }
+
+        { /* シート名(null) */
+            final Workbook workbook = ExcelUtils.getWorkbook(getResourceFile("testExcel/test.xlsx"));
+            final Sheet sheet = ExcelUtils.copySheet(workbook, "templateSheetName", null);
+
+            assertNull(sheet);
+        }
+
+        { /* シート名(null) */
+            final Workbook workbook = ExcelUtils.getWorkbook(getResourceFile("testExcel/test.xlsx"));
+            final Sheet sheet = ExcelUtils.copySheet(workbook, "Sheet1", null);
+
+            assertNull(sheet);
+        }
+
+        { /* シート名(空) */
+            final Workbook workbook = ExcelUtils.getWorkbook(getResourceFile("testExcel/test.xlsx"));
+            final Sheet sheet = ExcelUtils.copySheet(workbook, "Sheet1", "");
+
+            assertNull(sheet);
+        }
+
+        { /* 通常 */
+            final Workbook workbook = ExcelUtils.getWorkbook(getResourceFile("testExcel/test.xlsx"));
+            final int sheetNum = Objects.requireNonNull(workbook).getNumberOfSheets();
+            final Sheet sheet = ExcelUtils.copySheet(workbook, "Sheet1", "Sheet9");
+
+            assertNotNull(sheet);
+            assertEquals(sheet.getSheetName(), "Sheet9");
+            assertNotNull(workbook.getSheet("Sheet9"));
+            // コピー元は存在する。
+            assertNotNull(workbook.getSheet("Sheet1"));
+            assertEquals(workbook.getNumberOfSheets(), sheetNum + 1);
+        }
+    }
+
+    @Test
+    public void removeSheetTest_シートインデックス() {
+        { /* xls */
+            final Workbook workbook = ExcelUtils.getWorkbook(getResourceFile("testExcel/test.xls"));
+            final int sheetNum = Objects.requireNonNull(workbook).getNumberOfSheets();
+            ExcelUtils.removeSheet(workbook, 0);
+
+            assertNotNull(workbook);
+            assertEquals(workbook.getNumberOfSheets(), sheetNum - 1);
+            assertNull(ExcelUtils.getSheet(workbook, "Sheet1"));
+        }
+
+        { /* xls */
+            final Workbook workbook = ExcelUtils.getWorkbook(getResourceFile("testExcel/test-multi.xls"));
+            final int sheetNum = Objects.requireNonNull(workbook).getNumberOfSheets();
+            ExcelUtils.removeSheet(workbook, 0);
+
+            assertNotNull(workbook);
+            assertEquals(workbook.getNumberOfSheets(), sheetNum - 1);
+            assertNull(ExcelUtils.getSheet(workbook, "Sheet1"));
+            assertNotNull(ExcelUtils.getSheet(workbook, "Sheet2"));
+        }
+
+        { /* xlsx */
+            final Workbook workbook = ExcelUtils.getWorkbook(getResourceFile("testExcel/test.xlsx"));
+            final int sheetNum = Objects.requireNonNull(workbook).getNumberOfSheets();
+            ExcelUtils.removeSheet(workbook, 0);
+
+            assertNotNull(workbook);
+            assertEquals(workbook.getNumberOfSheets(), sheetNum - 1);
+            assertNull(ExcelUtils.getSheet(workbook, "Sheet1"));
+        }
+
+        { /* xlsx */
+            final Workbook workbook = ExcelUtils.getWorkbook(getResourceFile("testExcel/test-multi.xlsx"));
+            final int sheetNum = Objects.requireNonNull(workbook).getNumberOfSheets();
+            ExcelUtils.removeSheet(workbook, 0);
+
+            assertNotNull(workbook);
+            assertEquals(workbook.getNumberOfSheets(), sheetNum - 1);
+            assertNull(ExcelUtils.getSheet(workbook, "Sheet1"));
+            assertNotNull(ExcelUtils.getSheet(workbook, "Sheet2"));
+        }
+    }
 }
